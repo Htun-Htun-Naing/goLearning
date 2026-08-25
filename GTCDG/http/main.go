dab0099f9d70233166adfca864894393926d6a2b
htunnaing <kohtuntunnaing@gmail.com>
2026-08-25 11:19:14 +0630

    working with interfaces customize

--- GTCDG/http/main.go
+++ GTCDG/http/main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
-	resp, err := http.Get("http://www.google.com")
+	resp, err := http.Get("https://www.google.com")
 	if err != nil {
 		fmt.Println("Error:", err)
 		os.Exit(1)
 	}
-	fmt.Println(resp)
+	// fmt.Println(resp)
+	// preparing the empty byteslice with 9999999 spaces that served for
+	// the response data
+	// bs := make([]byte, 999999)
+	// // assigned the data into the bs variable
+	// resp.Body.Read(bs)
+	// //print out the data as a string
+	// fmt.Println(string(bs))
+	lw := logWriter{}
+	io.Copy(lw, resp.Body)
+
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
